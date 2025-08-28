e0d1d5951f2cf3b57a12d520ff2ba30862f728a0
AlgazinovAleksandr <algazinovalexandr@gmail.com>
2025-08-28 08:06:58 +0300

    Clean the repository

--- Demo UI Code/test.py
+++
@@ -1,4 +0,0 @@
-from transformers import BertForSequenceClassification
-model_path = "/Users/mattlaing/Desktop/TruthSeeker/models/finetuned"
-model = BertForSequenceClassification.from_pretrained(model_path)
-print("Model loaded successfully!")
